ce9f5e44d8a077e0d7b4e226c638c397d3b035a7
Christopher Ketant <ketantchris@gmail.com>
2022-10-24 11:35:43 -0500

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# fetch-rc-milestone
-GitHub Action to fetch the Release Candidate Milestone 
+# fetch-rc-milestones action
+
+This action works for a very specific usage for projects that leverage GitHub milestones for Release Candidate PR tracking. Such that each milestone represents a group of PRs that have been merged into `main` that will be in the Release Candidate. This action fetches the project milestone that represents the Release Candidate that is targeted for the upcoming production deployment.
+
+We determine the milestone by looking at it's **Due On** datetime as well as the string `release candidate` in the title. The Release Candidate found will either be the same day or in the future; never in the past.
+
+**Assumptions**
+1. You are generating Release Candidates in cadence.
+2. Employing the [Trunk-Based Development](https://www.atlassian.com/continuous-delivery/continuous-integration/trunk-based-development) Branching methodology.
+3. You group merged PRs (into `main`) in a milestone and a new Release Candidate will be cut from `main` targeted to `production`
+4. The Release Candidate milestones contains the substring *release candidate*
+
+
+## Development
+
+### Prerequisite
+You must have `ncc` installed in your global *node_modules* in order to build the project and not need to use local *node_modules* for the workflow. 
+
+1. `npm i -g @vercel/ncc` (*you may have to run as sudo*)
+
+### To Test Changes
+
+1. Run `npm run build && npm run start`
+
+### Committing
+
+After making source changes and before committing be sure to run `npm run build` then commit. The **dist/index.js** file is used to run this Action in order to avoid needing the **node_modules** directory. It builds the modules all in the **index.js** file.
+
+## Inputs
+
+### `repoOwner`
+
+**Required** The account owner of the repo
+
+### `repo`
+
+**Required** The name of the repo
+
+### `githubApiToken`
+
+**Required** GitHub API Token for accessing GitHub assets such as the tarball
+
+### `dueOnDate`
+
+**Optional** Find Release Candidate Milestone that has this due on date. If not provided then we find the upcoming RC Milestone. Expected format is YYYY-MM-DDTHH:mm:ss.sssZ ISO8601 format
+
+## Outputs
+
+### `milestone-title`
+
+Milestone title that represents the Release Candidate that will be going out 
+
+### `milestone-number`
+
+The unique number representing the milestone.
+
+### `milestone-id`
+
+The unique id representing the milestone.
+
+
+## Example usage
+```
+steps:
+      - name: Checkout Repo
+        uses: actions/checkout@v2
+      - name: Fetch milestones
+        uses: ./.github/actions/fetch-rc-milestones
+        with:
+          repoOwner: <OWNER-ACCOUNT-NAME>
+          repo: <REPO-NAME>
+          githubApiToken: ${{ secrets.GITHUB_API_TOKEN }}
+
+```
---
+++ index.js
@@ -0,0 +1,92 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+/**
+ * Get only the dates from the Date Object by setting
+ * the time to all zeros
+ * @param - Date object
+ * @return - Date object
+ * */
+const getDateWithoutTime = date => {
+	const dateWithoutTime = new Date(date.getTime())
+	dateWithoutTime.setUTCHours(0,0,0,0)
+	return dateWithoutTime
+}
+
+/**
+ * Get all the milestones for a project based on the query options
+ * https://octokit.github.io/rest.js/v18#issues-list-milestones
+ * 
+ * @param - OctoKit client
+ * @param - Object of query parameters
+ * @return - Async
+ * */
+const fetchMilestones = async (octoKit, queryOptions = {}) => {
+	return octoKit.rest.issues.listMilestones(queryOptions)
+}
+
+/**
+ * Get the milestone representing the Release Candidate that will be
+ * getting deployed next or the Release Candidate milestone with the
+ * same due date
+ * 
+ * @param - The substring to look for in the title of the milestone
+ * @param - Array of milestone objects
+ * @param - Optional Date string in ISO format
+ * @return - Milestone Object 
+ * */
+const getNextRCMilestone = (keySubstring, milestones, dueDate = null) => {
+	const targetDate = dueDate ? getDateWithoutTime(new Date(dueDate)) : getDateWithoutTime(new Date())
+	const filteredMilestones = milestones.filter((m) => {
+		const dueOn = getDateWithoutTime(new Date(m.due_on))
+		let isDateValid
+		if (dueDate) {
+			isDateValid = (dueOn.getTime() === targetDate.getTime()) // look for milestone with same due date
+		} else {
+			isDateValid = (dueOn >= targetDate) // for upcoming milestone
+		}
+		return isDateValid && m.title.toLowerCase().includes(keySubstring)
+	})
+	return filteredMilestones.length > 0 ? filteredMilestones[0] : null
+}
+
+const run = async () => {
+	try {
+
+		// Get Input
+		const token = core.getInput('githubApiToken')
+		const repo = core.getInput('repo')
+		const owner = core.getInput('repoOwner')
+		const dueDate = core.getInput('dueOnDate') ?? null
+
+		// Setup GH client
+		const octoKit = new github.getOctokit(token)
+
+		// Setup Params
+		const state = 'open'
+		const sort = 'due_on'
+		const direction = 'asc'
+
+		// 1. Fetch open milestones
+		const { data: allOpenMilestones } = await fetchMilestones(octoKit, {owner, repo, state, sort, direction})
+		console.log(`Successfully fetched ${allOpenMilestones.length} milestones`)
+		console.log('Filtering...')
+
+		// 2. Get the next Release Candidate Milestone that will be deployed next or that will be deployed on dueDate
+		const milestone = getNextRCMilestone('release candidate', allOpenMilestones, dueDate)
+		if(milestone) {
+			console.log(`Successfully found milestone: ${milestone.title}`)
+		} else {
+			console.log('Did not find any Release Candidate milestones')
+		}
+
+		// 3. Set Outputs
+		core.setOutput('milestone-title', milestone ? milestone.title : null)
+		core.setOutput('milestone-number', milestone ? milestone.number : null)
+		core.setOutput('milestone-id', milestone ? milestone.id : null)
+	} catch (error) {
+		core.setFailed(error)
+	}
+}
+
+run()
